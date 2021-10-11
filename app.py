3833db309eda1782d24de647a47610758a89e1bc
BesnikShala <bes.shala@gmail.com>
2021-10-11 20:56:37 +0000

    Add register password confirm functionality and add log in functionality

--- app.py
+++ app.py
@@ -36,6 +36,12 @@ def register():
         if existing_user:
             flash("This username is already in use, please try another")
             return redirect(url_for("register"))
+        # confirm password
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm-pass")
+        if password != confirm_password:
+            flash("The password doesn't match")
+            return redirect(url_for("register"))
 
         register = {
             "username": request.form.get("username").lower(),
@@ -46,6 +52,28 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("You Have Successfully Registered")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
