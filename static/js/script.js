737ffbfcf22ae35067fcfe56972df6e22102e9ed
BesnikShala <bes.shala@gmail.com>
2021-10-13 17:35:08 +0000

    Add form to add_recipe.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,21 @@
 //  initialize javascript
- $(document).ready(function () {
-     $('.sidenav').sidenav({edge: "right"});
-     $('.modal').modal();
- });
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: "right"
+    });
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('select').formSelect();
+    $('.chips-initial').chips({
+        autocompleteOptions: {
+            //  data: [{
+            //      tag: 'e.g carrots'
+            //  }],
+        },
+        onChipAdd: function (autocomplete, tag) {
+            $(tag).find('.material-icons').removeClass('material-icons').addClass('fas fa-times right').text('');
+            $(".chip").attr("name", "ingredients");
+        }
+    });
+
+});
